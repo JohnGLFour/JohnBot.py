d8b9b38a68405d2a1471d3802e17561684768f62
JohnGLFour <johnharrylau@gmail.com>
2018-08-04 11:57:50 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,23 +4,30 @@ from discord.ext.commands import Bot
 import asyncio
 import os
 
-
-
-bot = commands.Bot(command_prefix="a.")
-
-
+bot = commands.Bot(command_prefix="jb!")
 
 @bot.event
 async def on_ready():
-  print(bot.user.name)
+    print ("Ready when you are xd")
+    print ("I am running on " + bot.user.name)
+    print ("With the ID: " + bot.user.id)
 
-  
-  
 @bot.command(pass_context=True)
-async def hi(ctx):
-  await bot.say("Hello there"+" "+ctx.message.author.name)
-  
-  
-  
-  
-bot.run(os.environ['BOT_TOKEN'])
+async def ping(ctx):
+    await bot.say(":ping_pong: ping!! xSSS")
+    print ("user has pinged")
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    await bot.say("The users name is: {}".format(user.name))
+    await bot.say("The users ID is: {}".format(user.id))
+    await bot.say("The users status is: {}".format(user.status))
+    await bot.say("The users highest role is: {}".format(user.top_role))
+    await bot.say("The user joined at: {}".format(user.joined_at))
+
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member):
+    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
+    await bot.kick(user)
+
+bot.run(os.environ['token'])
